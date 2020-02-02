91d4101457499a706d765d23158c27c092587baa
aresnik11 <aresnik11@gmail.com>
2020-02-02 17:54:30 -0500

    seeded pokemon

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,6 @@
+require 'rest-client'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..150 do
+    response_string = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{i}")
+    pokemon = JSON.parse(response_string)
+    Pokemon.create!(
+        name: pokemon["name"],
+        height: pokemon["height"],
+        weight: pokemon["weight"],
+        front_img: pokemon["sprites"]["front_default"],
+        back_img: pokemon["sprites"]["back_default"]
+    )
+end
