6b52459af8b052004bbd1483d597541976b973ce
aresnik11 <aresnik11@gmail.com>
2020-02-02 19:50:45 -0500

    connected frontend to backend to get data via fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    pokemon: []
+  }
+  
   componentDidMount() {
-    fetch('http://localhost:3001')
+    fetch('http://localhost:3001/pokemons')
     .then(resp => resp.json())
     .then(data => {
-      console.log(data)
+      this.setState({
+        pokemon: data
+      })
     })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1>Welcome to the Pokemon App</h1>
