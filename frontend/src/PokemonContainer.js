1e5ec5c76ac1959be5aa90ca2244a9a6ab009a21
aresnik11 <aresnik11@gmail.com>
2020-02-03 11:10:20 -0500

    added pokemon container

---
+++ frontend/src/PokemonContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PokemonContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokemon Container</h1>
+      </div>
+    )
+  }
+
+}
+
+export default PokemonContainer;
