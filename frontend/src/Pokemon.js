d805290b5e1b97bd936591a6ade175cd609ca4a2
aresnik11 <aresnik11@gmail.com>
2020-02-07 12:15:48 -0500

    render kinds and moves for each pokemon

--- frontend/src/Pokemon.js
+++ frontend/src/Pokemon.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const Pokemon = (props) => {
-    console.log(props)
+    const getMoves = () => {
+        return props.pokemon.moves.map(move => move.name).join(", ")
+    }
+
+    const getKinds = () => {
+        return props.pokemon.kinds.map(kind => kind.name).join(", ")
+    }
+
     return (
         <div className="pokemon">
             <h1>{props.pokemon.name}</h1>
             <img src={props.pokemon.front_img} alt={props.pokemon.name} />
             <h4>Height: {props.pokemon.height}</h4>
             <h4>Weight: {props.pokemon.weight}</h4>
+            <h4>Type: {getKinds()}</h4>
+            <h4>Moves: {getMoves()}</h4>
         </div>
     )
 }
