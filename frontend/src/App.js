1716ac79729dcc35ab0b94abd2fc9147e1d263c9
aresnik11 <aresnik11@gmail.com>
2020-02-07 12:11:15 -0500

    added loading messaging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Welcome to the Pokemon App</h1>
+        {this.state.pokemon.length
+        ?
         <PokemonContainer pokemon={this.state.pokemon} />
+        :
+        <h1>Loading...</h1>
+        }
       </div>
     )
   }
