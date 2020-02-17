cd0a889599c47b98531ed79a13abdeafcdb08c77
aresnik11 <aresnik11@gmail.com>
2020-02-17 13:41:12 -0500

    updated pokemon name

--- frontend/src/Pokemon.js
+++ frontend/src/Pokemon.js
@@ -11,7 +11,7 @@ const Pokemon = (props) => {
 
     return (
         <div className="pokemon">
-            <h1>{props.pokemon.name}</h1>
+            <h1>{props.pokemon.name}!</h1>
             <img src={props.pokemon.front_img} alt={props.pokemon.name} />
             <h4>Height: {props.pokemon.height}</h4>
             <h4>Weight: {props.pokemon.weight}</h4>
